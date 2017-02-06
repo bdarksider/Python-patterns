1f1d35b1b0dda70672dc280accaff3d7aee907ef
Bejoy Mathew <bejoy.mathew@geminisolutions.in>
2017-02-07 00:35:13 +0530

    lazy_evaluation completed

---
+++ creational/lazy_evaluation.py
@@ -0,0 +1,44 @@
+"""lazily-evaluated property pattern in python"""
+
+import functools
+
+
+class lazy_property(object):
+
+    def __init__(self, function):
+        self.function = function
+        functools.update_wrapper(self, function)
+
+    def __get__(self, obj, type_):
+        if obj is None:
+            return self
+        val = self.function(obj)
+        obj.__dict__[self.function.__name__] = val
+        return val
+
+
+class Person(object):
+
+    def __init__(self, name, occupation):
+        self.name = name
+        self.occupation = occupation
+
+    @lazy_property
+    def relatives(self):
+        # Get all relatives, let's assume that it costs much time.
+        relatives = "Many relatives."
+        return relatives
+
+
+def main():
+    Jhon = Person('Jhon', 'Coder')
+    print("Name: {0}    Occupation: {1}".format(Jhon.name, Jhon.occupation))
+    print("Before we access `relatives`:")
+    print(Jhon.__dict__)
+    print("Jhon's relatives: {0}".format(Jhon.relatives))
+    print("After we've accessed `relatives`:")
+    print(Jhon.__dict__)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,11 @@
+def parent():
+    print("Printing from the parent()")
+
+    def frist_child():
+        return "First_child()"
+    def second_child():
+        return "Second_child()"
+
+    print(first_child())
+    print(second_child())
+
